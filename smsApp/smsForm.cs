465471b13a7ad9e3acb9a7e8bf9a1e819993ebdd
Jonathan Narzo <jonathan.narzo@gmail.com>
2014-10-19 12:30:30 +0800

    Finalized sms app ui design

--- smsApp/smsForm.cs
+++ smsApp/smsForm.cs
@@ -7,14 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace smsApp
 {
     public partial class smsForm : Form
     {
+        SerialPort port = new SerialPort();
+        smsMethods objsendSMS = new smsMethods();
+        ShortMessageCollection objShortMessageCollection = new ShortMessageCollection();
+        string baudRate = "9600";
         public smsForm()
         {
             InitializeComponent();
         }
+
+        private void smsForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] ports = SerialPort.GetPortNames();
+                foreach (string port in ports)
+                {
+                    this.cbPorts.Items.Add(port);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.port = objsendSMS.OpenPort(this.cbPorts.Text, baudRate);
+
+                if (this.port != null)
+                {
+                    this.lblStatus.Text = "Connected at " + this.cbPorts.Text;
+                    cbPorts.Enabled = false;
+                    btnConnect.Enabled = false;
+                    btnDisconnect.Enabled = true;
+                    tabControls.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid port settings");
+                    this.lblStatus.Text = "Not Connected";
+                    cbPorts.Enabled = true;
+                    btnConnect.Enabled = true;
+                    btnDisconnect.Enabled = false;
+                    tabControls.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objsendSMS.ClosePort(this.port);
+                this.lblStatus.Text = "Not Connected";
+                cbPorts.Enabled = true;
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                tabControls.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
     }
 }
