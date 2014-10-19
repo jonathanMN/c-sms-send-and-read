291815a66f722c0ab726715feebad641a96ccd69
Jonathan Narzo <jonathan.narzo@gmail.com>
2014-10-19 12:32:50 +0800

    Added other auto generated files

--- smsApp/smsForm.Designer.cs
+++ smsApp/smsForm.Designer.cs
@@ -28,12 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControls = new System.Windows.Forms.TabControl();
+            this.Tabsendsms = new System.Windows.Forms.TabPage();
+            this.tabReadsms = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lvMessage = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControls.SuspendLayout();
+            this.Tabsendsms.SuspendLayout();
+            this.tabReadsms.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControls
+            // 
+            this.tabControls.Controls.Add(this.Tabsendsms);
+            this.tabControls.Controls.Add(this.tabReadsms);
+            this.tabControls.Enabled = false;
+            this.tabControls.Location = new System.Drawing.Point(13, 68);
+            this.tabControls.Name = "tabControls";
+            this.tabControls.SelectedIndex = 0;
+            this.tabControls.Size = new System.Drawing.Size(523, 226);
+            this.tabControls.TabIndex = 0;
+            // 
+            // Tabsendsms
+            // 
+            this.Tabsendsms.Controls.Add(this.label2);
+            this.Tabsendsms.Controls.Add(this.label1);
+            this.Tabsendsms.Controls.Add(this.btnSend);
+            this.Tabsendsms.Controls.Add(this.textBox2);
+            this.Tabsendsms.Controls.Add(this.textBox1);
+            this.Tabsendsms.Location = new System.Drawing.Point(4, 22);
+            this.Tabsendsms.Name = "Tabsendsms";
+            this.Tabsendsms.Padding = new System.Windows.Forms.Padding(3);
+            this.Tabsendsms.Size = new System.Drawing.Size(515, 200);
+            this.Tabsendsms.TabIndex = 0;
+            this.Tabsendsms.Text = "Send Message";
+            this.Tabsendsms.UseVisualStyleBackColor = true;
+            // 
+            // tabReadsms
+            // 
+            this.tabReadsms.Controls.Add(this.lvMessage);
+            this.tabReadsms.Location = new System.Drawing.Point(4, 22);
+            this.tabReadsms.Name = "tabReadsms";
+            this.tabReadsms.Padding = new System.Windows.Forms.Padding(3);
+            this.tabReadsms.Size = new System.Drawing.Size(515, 200);
+            this.tabReadsms.TabIndex = 1;
+            this.tabReadsms.Text = "Read Messages";
+            this.tabReadsms.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 79);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(482, 78);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(218, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(16, 163);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Phone Number :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Message :";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Blue;
+            this.lblStatus.Location = new System.Drawing.Point(49, 304);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(116, 20);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Not Connected";
+            // 
+            // lvMessage
+            // 
+            this.lvMessage.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvMessage.FullRowSelect = true;
+            this.lvMessage.Location = new System.Drawing.Point(14, 18);
+            this.lvMessage.Name = "lvMessage";
+            this.lvMessage.Size = new System.Drawing.Size(486, 169);
+            this.lvMessage.TabIndex = 0;
+            this.lvMessage.UseCompatibleStateImageBehavior = false;
+            this.lvMessage.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Index";
+            this.columnHeader1.Width = 41;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Sent Time";
+            this.columnHeader2.Width = 89;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "From";
+            this.columnHeader3.Width = 131;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Message";
+            this.columnHeader4.Width = 135;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Select Port :";
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(80, 14);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(71, 21);
+            this.cbPorts.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(154, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(231, 13);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Status :";
+            // 
+            // smsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(547, 337);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cbPorts);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.tabControls);
+            this.Name = "smsForm";
+            this.Text = "SMS Appication";
+            this.Load += new System.EventHandler(this.smsForm_Load);
+            this.tabControls.ResumeLayout(false);
+            this.Tabsendsms.ResumeLayout(false);
+            this.Tabsendsms.PerformLayout();
+            this.tabReadsms.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControls;
+        private System.Windows.Forms.TabPage Tabsendsms;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabReadsms;
+        private System.Windows.Forms.ListView lvMessage;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label label4;
     }
 }
 
